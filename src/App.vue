56c186517254c112da94042b805c049940bea36c
Mchtsz <mhatland206@gmail.com>
2023-11-01 11:02:23 +0100

    git init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+import './assets/main.css';
+
+</script>
+
+<template>
+  <NavBar />
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup lang=ts>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <h1 data-navLink data-white>
+            MH
+        </h1>
+        
+        <main>
+            <RouterLink to="/" data-navLink>home</RouterLink>
+            <RouterLink to="/about" data-navLink>about us</RouterLink>
+            <RouterLink to="/contact" data-navLink>contact</RouterLink>
+        </main>
+    </nav>
+</template>
+
+<style scoped lang=scss>
+nav {
+    width: 100%;
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 2rem;
+
+    main {
+        display: flex;
+        gap: 2rem;
+    }
+
+    h1 {
+        margin: auto 0;
+    }
+}
+</style>
