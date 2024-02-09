603d526f0600a98d533f2b76112f383e0872d2c7
Mchtsz <mhatland206@gmail.com>
2024-02-09 16:01:25 +0100

    super

--- .output/public/_nuxt/index.86ab36a7.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,o as c,a as e}from"./entry.5518bda9.js";const o=""+globalThis.__publicAssetsURL("search-engine.svg"),t={},_=e("img",{src:o,alt:"Search-engine"},null,-1),a=e("h1",null,"Hello",-1),l=[_,a];function r(i,d){return c(),n("main",null,l)}const m=s(t,[["render",r]]);export{m as default};
---
+++ .output/public/_nuxt/info.30d07de2.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./entry.dd403ad2.js";const r={};function c(e,t){return null}const o=n(r,[["render",c]]);export{o as default};
