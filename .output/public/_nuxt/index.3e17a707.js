fa41240fd0f57bc4443ae0a1cb74eb24c16eab47
Mchtsz <mhatland206@gmail.com>
2024-02-22 13:28:20 +0100

    removed all

--- .output/public/_nuxt/index.3e17a707.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.0c40a31d.js";const o={};function r(t,a){return n(),c("main")}const _=e(o,[["render",r]]);export{_ as default};
--- .output/public/_nuxt/info.347886c1.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./entry.0c40a31d.js";const r={};function c(e,t){return null}const o=n(r,[["render",c]]);export{o as default};
