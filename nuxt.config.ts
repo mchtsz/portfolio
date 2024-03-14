52c95bacc4a93e732c325648d6749b990bdaf28e
Mchtsz <mhatland206@gmail.com>
2024-03-14 15:27:56 +0100

    let me cook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// https://fonts.google.com/icons
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/styles/global.scss'],
+  ssr: false,
+  modules: [
+    'nuxt-quasar-ui'
+  ],
+  quasar: { 
+    config: {
+      brand: {
+        primary: '#FF5722',
+        secondary: '#03A9F4',
+        accent: '#00BCD4',
+        dark: '#212121',
+        positive: '#4CAF50',
+        negative: '#FF5252',
+        info: '#2196F3',
+        warning: '#FFC107',
+      }
+    },
+
+    iconSet: 'material-symbols-rounded',
+    extras: {
+      svgIcons: [
+        "material-symbols-rounded",
+      ]
+    }
+
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,56 @@
-<script setup lang=ts>
+<script setup lang="ts">
 
+const profile = ref({
+    name: 'Marthin Hatland',
+    title: 'Frontend Developer',
+    btntext: 'Kontakt meg'
+})
 </script>
 
 <template>
-    
+    <main>
+        <div class="box">
+            <h1>{{ profile.name }}</h1>
+            <p>{{ profile.title }}</p>
+            <button>{{ profile.btntext }}</button>
+        </div>
+    </main>
 </template>
 
-<style scoped lang=scss>
+<style scoped lang="scss">
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 
+    .box {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        text-align: center;
+        align-items: center;
+        width: 100%;
+        h1 {
+            font-size: 4rem;
+        }
+        p {
+            font-size: 2rem;
+        }
+        button {
+            border-radius: 2rem;
+            padding: 1rem 2rem;
+            width: 200px;
+            font-size: 1.2rem;
+            border: none;
+            background-color: #000;
+            color: #fff;
+            cursor: pointer;
+
+            &:hover {
+                background-color: #333;
+            }
+        }
+    }
+}
 </style>
